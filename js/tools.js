b231e84bfb10cdd04adf45114c18e0612fcb13ec
Draho <drahoslav@yo2.cz>
2021-04-29 15:07:33 +0200

    Fix ondemandRandomList

--- js/tools.js
+++ js/tools.js
@@ -1,9 +1,11 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
 
 const rand = (n) => Math.floor(Math.random()*n)
 
-
 // returns pair of +- ones to indicate corner
-// indexed from top left goin clockwise
+// indexed from top left goin clockwise: --, -+, ++, +- 
 const indexToCorner = (i) => {
   i+=2
   i%=4
@@ -20,15 +22,13 @@ const wait = async (t) => new Promise((resolve) => {
   setTimeout(resolve, t)
 })
 
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-
 const ondemandRandomList = (values) => {
+  const len = values.length
   let known = []
   return new Proxy(known, {
     get: (list, i) => {
       if (i === 'length') {
-        return values.length + known.length
+        return len
       }
       if (!(i in known)) {
         values.sort(randomize)
